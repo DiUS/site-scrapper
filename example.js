814368f3d6353e10ef709254f5e226493dd8ac7f
antklim <antklim@gmail.com>
2016-06-22 12:08:04 +1000

    eslint added

--- example.js
+++ example.js
@@ -3,7 +3,9 @@ const scrapper = require('./src/scrapper')
 const baseUrl = 'https://www.reddit.com/'
 const sitePages = require('./site-map').pages
 
+/* eslint-disable no-console */
 scrapper.scrapePagesList(baseUrl, sitePages, (err, pages) => {
   if (err) return console.error(err)
   console.log(pages)
 })
+/* eslint-enable no-console */
